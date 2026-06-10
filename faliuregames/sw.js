dcd31a5f61a326d47a1d40b6d20e399cec0460d5
CyberWolfYT <superamanwaris@gmail.com>
2026-06-10 19:32:40 +0530

    Add files via upload

---
+++ faliuregames/sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'failure-games-v5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-512.png'
+];
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }).then(function () { return self.skipWaiting(); }));
+});
+self.addEventListener('activate', function (e) {
+  e.waitUntil(caches.keys().then(function (ks) {
+    return Promise.all(ks.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      return r || fetch(e.request).then(function (resp) {
+        var cp = resp.clone();
+        caches.open(CACHE).then(function (c) { c.put(e.request, cp); });
+        return resp;
+      }).catch(function () { return caches.match('./index.html'); });
+    })
+  );
+});
